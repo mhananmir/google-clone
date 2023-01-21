d679e00ed134b006122860dfb60b80457dc25be2
Hanan Mir <mhananm33r@gmail.com>
2023-01-21 03:55:26 -0800

    Updates

---
+++ precache-manifest.11921977ea05d244ec293ecc74d4f2f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d96821888acd39328727",
+    "url": "/google-clone/static/css/main.1365f5d7.chunk.css"
+  },
+  {
+    "revision": "d96821888acd39328727",
+    "url": "/google-clone/static/js/main.d9682188.chunk.js"
+  },
+  {
+    "revision": "2b4e57923141e0aa228a",
+    "url": "/google-clone/static/js/1.2b4e5792.chunk.js"
+  },
+  {
+    "revision": "1e22ad3ef0aab3805b67",
+    "url": "/google-clone/static/js/2.1e22ad3e.chunk.js"
+  },
+  {
+    "revision": "564873a24285e740616f",
+    "url": "/google-clone/static/js/runtime~main.564873a2.js"
+  },
+  {
+    "revision": "3380d67cdd22f43207368418f5a38402",
+    "url": "/google-clone/index.html"
+  }
+];
